6c7e357ff8d3b2b80d44ae8f8e7eff22439d19b2
ahmedhamdy749 <112814634+ahmedhamdy749@users.noreply.github.com>
2022-12-04 01:36:57 +0200

    Create get-review.py

---
+++ functions/get-review.py
@@ -0,0 +1,8 @@
+"""
+I did not use this file at all, since I repurposed the existing functions...
+So you may want to check out the pass-the-docs.js and format-docs.js files.
+"""
+import sys
+
+def main(dict):
+    return { 'message': 'Hello world' }
